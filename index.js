1d30081e86711dfaf6ff105b261e64dcc6861763
Christian Caballari <christianccm17@gmail.com>
2023-10-31 23:16:16 -0600

    Fin sección 10 - Auth

--- index.js
+++ index.js
@@ -2,21 +2,30 @@ const express = require("express");
 require("dotenv").config();
 const { dbConnection } = require("./database/config");
 const cors = require("cors");
+
 //Crear el servidor de express
 const app = express();
+
 //Configurar cors
 app.use(cors());
+
+// Lectura y parseo del body
+app.use(express.json());
+
+app.use(express.urlencoded({
+  extended:true
+  }));
 //Base de datos
 dbConnection();
+app.use('/api/usuarios',require('./routes/usuarios'));
+app.use('/api/login',require('./routes/auth'));
+
+
+
+
 //password : mqLoeiZyv5cRiPw3
 //user: mean_user
 //Rutas
-app.get("/", (req, res) => {
-  res.json({
-    ok: true,
-    msg: "Hola Mundo",
-  });
-});
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
 });
